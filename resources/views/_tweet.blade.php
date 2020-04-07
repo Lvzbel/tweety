357d05259b836c3ea99ac7468dc8f053bf98b97b
Lvzbel <rcoto@protonmail.com>
2020-04-06 20:14:44 -0700

    Feat: timeline design

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,13 @@
+<div class="flex p-4 border-b border-b-gray-400">
+  <div class="mr-2 flex-shrink-0">
+      <img src="https://i.pravatar.cc/50" alt="" class="rounded-full mr-2">
+  </div>
+
+  <div>
+      <h5 class="font-bold mb-4">Jonh Doe</h5>
+
+      <p class="text-sm">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam quas quibusdam quo laboriosam dolorem magnam cumque velit quae vitae, porro eos culpa voluptatibus ipsa consequatur amet ducimus neque saepe laudantium?
+      </p>
+  </div>
+</div>
